366358e168b144321556ca1cd01e47c6a5721032
Bartosz Bartosik <babosz189@gmail.com>
2023-10-17 23:57:08 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import numpy as np
+from mdp import MDP
+from qlearning import QLearning
+
+def main():
+
+    def reward_function(env, valid):
+        if valid:
+            if env[0, 1] == 1:
+                return 10
+            return 0
+        return -10
+
+    environment = np.array([[0, 3, 0, 0, 0],
+                                [0, 0, 0, 0, 0],
+                                [2, 2, 2, 0, 0],
+                                [1, 0, 0, 0, 0]])
+
+
+    actions = ('l', 'r', 'u', 'd')
+    episodes = 5
+
+    mdp = MDP(environment, actions, episodes, 0.7, reward_function, agent_encoding=1, goal_encoding=3, obstacle_encoding=2)
+
+
+    print('Environment:\n{}'.format(mdp.environment))
+    print('Actions: {}'.format(mdp.actions))
+    print('Current agent position: {}'.format(mdp.environment[mdp.get_position('agent')]))
+
+    print(mdp.qtable)
+
+    print('Action reward: {}'.format(mdp.take_action('r')))
+    print('Environment:\n{}'.format(mdp.environment))
+    print('Action reward: {}'.format(mdp.take_action('r')))
+    print('Environment:\n{}'.format(mdp.environment))
+    print('Action reward: {}'.format(mdp.take_action('r')))
+    print('Environment:\n{}'.format(mdp.environment))
+    print('Action reward: {}'.format(mdp.take_action('u')))
+    print('Environment:\n{}'.format(mdp.environment))
+    print('Action reward: {}'.format(mdp.take_action('u')))
+    print('Environment:\n{}'.format(mdp.environment))
+    print('Action reward: {}'.format(mdp.take_action('u')))
+    print('Environment:\n{}'.format(mdp.environment))
+    print('Action reward: {}'.format(mdp.take_action('l')))
+    print('Environment:\n{}'.format(mdp.environment))
+    print('Action reward: {}'.format(mdp.take_action('l')))
+    print('Environment:\n{}'.format(mdp.environment))
+
+    ql = QLearning(mdp)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ mdp.py
@@ -0,0 +1,78 @@
+import random
+
+import numpy as np
+
+
+class MDP:
+
+    def __init__(self, environment, actions, episodes, learning_rate, reward_function, agent_encoding, obstacle_encoding, goal_encoding):
+        self.environment = environment
+        self.actions = actions
+        self.episodes = episodes
+        self.learning_rate = learning_rate
+        self.reward_function = reward_function
+        self.agent_encoding = agent_encoding
+        self.obstacle_encoding = obstacle_encoding
+        self.goal_encoding = goal_encoding
+
+
+    def get_position(self, feature):
+        match feature:
+            case 'agent':
+                item = self.agent_encoding
+            case 'obstacle':
+                item = self.obstacle_encoding
+            case 'goal_encoding':
+                item = self.goal_encoding
+            case _:
+                raise Exception('Error while fetching a position - no such item as "{}".'.format(feature))
+        for position, i in np.ndenumerate(self.environment):
+            if i == item:
+                return position
+
+
+    def get_reward(self, valid):
+        return self.reward_function(self.environment, valid)
+
+
+    def encode_action(self, action):
+        environment = self.environment
+        x, y = self.get_position('agent')
+
+
+        match action:
+            case 'l':
+                return x, y - 1
+            case 'r':
+                return x, y + 1
+            case 'u':
+                return x - 1, y
+            case 'd':
+                return x + 1, y
+            case _:
+                return x, y
+
+
+    def take_action(self, action):
+        action_encoded = self.encode_action(action)
+        agent_position = self.get_position('agent')
+
+        is_valid = self.validate_action(action)
+
+        if is_valid:
+            self.environment[action_encoded], self.environment[agent_position] = (
+                self.environment[agent_position], self.environment[action_encoded])
+
+        return self.get_reward(is_valid)
+
+
+    def validate_action(self, action):
+        x_new, y_new = action_encoded = self.encode_action(action)
+
+        if (x_new < 0 or x_new >= len(self.environment) or
+                y_new < 0 or y_new >= len(self.environment[0]) or
+                self.environment[action_encoded] == self.obstacle_encoding):
+            return False
+
+        return True
+
