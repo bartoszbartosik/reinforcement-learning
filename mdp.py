dea557c8d972b37a02d9bd37f34b7cae883437d5
Bartosz Bartosik <babosz189@gmail.com>
2023-10-19 19:42:32 +0200

    Privatized functions in the MDP class

--- mdp.py
+++ mdp.py
@@ -22,11 +22,38 @@ class MDP:
                 return position
 
 
-    def get_reward(self, valid):
+    def take_action(self, action):
+        action_encoded = self.__encode_action(action)
+        agent_position = self.get_agent_position()
+
+        is_valid = self.__validate_action(action)
+
+        if is_valid:
+            self.environment[action_encoded], self.environment[agent_position] = (
+                self.environment[agent_position], self.environment[action_encoded])
+
+        return self.__get_reward(is_valid)
+
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # # # # # # # # # # # # # # # # # # # # #   PRIVATE   FUNCTIONS   # # # # # # # # # # # # # # # # # # # # #
+
+    def __get_reward(self, valid):
         return self.reward_function(self.environment, valid)
 
 
-    def encode_action(self, action):
+    def __validate_action(self, action):
+        x_new, y_new = action_encoded = self.__encode_action(action)
+
+        if (x_new < 0 or x_new >= len(self.environment) or
+                y_new < 0 or y_new >= len(self.environment[0]) or
+                self.environment[action_encoded] == self.obstacle_encoding):
+            return False
+
+        return True
+
+
+    def __encode_action(self, action):
         x, y = self.get_agent_position()
 
 
@@ -41,28 +68,4 @@ class MDP:
                 return x + 1, y
             case _:
                 return x, y
-
-
-    def take_action(self, action):
-        action_encoded = self.encode_action(action)
-        agent_position = self.get_agent_position()
-
-        is_valid = self.validate_action(action)
-
-        if is_valid:
-            self.environment[action_encoded], self.environment[agent_position] = (
-                self.environment[agent_position], self.environment[action_encoded])
-
-        return self.get_reward(is_valid)
-
-
-    def validate_action(self, action):
-        x_new, y_new = action_encoded = self.encode_action(action)
-
-        if (x_new < 0 or x_new >= len(self.environment) or
-                y_new < 0 or y_new >= len(self.environment[0]) or
-                self.environment[action_encoded] == self.obstacle_encoding):
-            return False
-
-        return True
 
