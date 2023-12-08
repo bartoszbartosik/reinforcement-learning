bb183242fbc83022d68c30291d9f24e1ce77dcf9
Bartosz Bartosik <babosz189@gmail.com>
2023-12-08 15:06:47 +0100

    Added actions and their validation to grid_env class

--- grid_env.py
+++ grid_env.py
@@ -20,14 +20,61 @@ class GridEnv:
         self.width = width      # columns
         self.height = height    # rows
 
-        self.grid = GridElements.FREE*np.ones(shape=(height, width))
+        self.grid = GridElements.FREE.value*np.ones(shape=(height, width))
+
+        self.agent_pos = [0, 0]
 
 
-    def set_agent(self, agent_position: tuple):
-        self.grid[agent_position] = GridElements.AGENT
+    def action(self, action: AgentActions):
+
+        if self.validate_action(action):
+            old_pos = self.agent_pos.copy()
+            match action:
+                case AgentActions.UP:
+                    self.agent_pos[0] -= 1
+                case AgentActions.DOWN:
+                    self.agent_pos[0] += 1
+                case AgentActions.LEFT:
+                    self.agent_pos[1] -= 1
+                case AgentActions.RIGHT:
+                    self.agent_pos[1] += 1
+            print('old_pos', old_pos, 'new_pos', self.agent_pos)
+            self.grid[tuple(self.agent_pos)], self.grid[tuple(old_pos)] = self.grid[tuple(old_pos)], self.grid[tuple(self.agent_pos)]
+            return True
+        return False
+
+
+    def validate_action(self, action):
+
+        # Check for walls
+        if self.agent_pos[0] == 0 and action == AgentActions.UP:
+            return False
+        elif self.agent_pos[0] == len(self.grid)-1 and action == AgentActions.DOWN:
+            return False
+        elif self.agent_pos[1] == 0 and action == AgentActions.LEFT:
+            return False
+        elif self.agent_pos[1] == len(self.grid[0])-1 and action == AgentActions.RIGHT:
+            return False
+        # Check for obstacles
+        elif action == AgentActions.UP and self.grid[self.agent_pos[0]-1, self.agent_pos[1]] == GridElements.OBSTACLE.value:
+            return False
+        elif action == AgentActions.DOWN and self.grid[self.agent_pos[0]+1, self.agent_pos[1]] == GridElements.OBSTACLE.value:
+            return False
+        elif action == AgentActions.LEFT and self.grid[self.agent_pos[0], self.agent_pos[1]-1] == GridElements.OBSTACLE.value:
+            return False
+        elif action == AgentActions.RIGHT and self.grid[self.agent_pos[0], self.agent_pos[1]+1] == GridElements.OBSTACLE.value:
+            return False
+        return True
+
+
+    def set_agent(self, agent_position: list):
+        self.agent_pos = agent_position
+        self.grid[tuple(agent_position)] = GridElements.AGENT.value
+
 
     def set_obstacle(self, obstacle_position: tuple):
-        self.grid[obstacle_position] = GridElements.OBSTACLE
+        self.grid[obstacle_position] = GridElements.OBSTACLE.value
+
 
     def set_reward(self, reward_position: tuple):
-        self.grid[reward_position] = GridElements.REWARD
+        self.grid[reward_position] = GridElements.REWARD.value
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 from mdp import MDP
-from grid_env import GridEnv, GridElements
+from grid_env import GridEnv, GridElements, AgentActions
 
 def main():
 
@@ -20,8 +20,11 @@ def main():
     grid.set_obstacle((2,0))
     grid.set_obstacle((2,1))
     grid.set_obstacle((2,2))
-    grid.set_agent((3,0))
+    grid.set_agent([3,0])
     grid.set_reward((0,1))
+
+    print(grid.grid)
+    print(grid.action(AgentActions.RIGHT))
     print(grid.grid)
 
     actions = ('l', 'r', 'u', 'd')
@@ -30,26 +33,26 @@ def main():
     mdp = MDP(environment, actions, reward_function, agent_encoding=1, obstacle_encoding=2)
 
 
-    print('Environment:\n{}'.format(mdp.environment))
-    print('Actions: {}'.format(mdp.actions))
-    print('Current agent position: {}'.format(mdp.environment[mdp.get_agent_position()]))
-
-    print('Action reward: {}'.format(mdp.take_action('r')))
-    print('Environment:\n{}'.format(mdp.environment))
-    print('Action reward: {}'.format(mdp.take_action('r')))
-    print('Environment:\n{}'.format(mdp.environment))
-    print('Action reward: {}'.format(mdp.take_action('r')))
-    print('Environment:\n{}'.format(mdp.environment))
-    print('Action reward: {}'.format(mdp.take_action('u')))
-    print('Environment:\n{}'.format(mdp.environment))
-    print('Action reward: {}'.format(mdp.take_action('u')))
-    print('Environment:\n{}'.format(mdp.environment))
-    print('Action reward: {}'.format(mdp.take_action('u')))
-    print('Environment:\n{}'.format(mdp.environment))
-    print('Action reward: {}'.format(mdp.take_action('l')))
-    print('Environment:\n{}'.format(mdp.environment))
-    print('Action reward: {}'.format(mdp.take_action('l')))
-    print('Environment:\n{}'.format(mdp.environment))
+    # print('Environment:\n{}'.format(mdp.environment))
+    # print('Actions: {}'.format(mdp.actions))
+    # print('Current agent position: {}'.format(mdp.environment[mdp.get_agent_position()]))
+    #
+    # print('Action reward: {}'.format(mdp.take_action('r')))
+    # print('Environment:\n{}'.format(mdp.environment))
+    # print('Action reward: {}'.format(mdp.take_action('r')))
+    # print('Environment:\n{}'.format(mdp.environment))
+    # print('Action reward: {}'.format(mdp.take_action('r')))
+    # print('Environment:\n{}'.format(mdp.environment))
+    # print('Action reward: {}'.format(mdp.take_action('u')))
+    # print('Environment:\n{}'.format(mdp.environment))
+    # print('Action reward: {}'.format(mdp.take_action('u')))
+    # print('Environment:\n{}'.format(mdp.environment))
+    # print('Action reward: {}'.format(mdp.take_action('u')))
+    # print('Environment:\n{}'.format(mdp.environment))
+    # print('Action reward: {}'.format(mdp.take_action('l')))
+    # print('Environment:\n{}'.format(mdp.environment))
+    # print('Action reward: {}'.format(mdp.take_action('l')))
+    # print('Environment:\n{}'.format(mdp.environment))
 
 
 if __name__ == '__main__':
