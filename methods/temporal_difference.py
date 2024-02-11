ef71e49d0123e7a12474caf78a5e401576072adc
Bartosz Bartosik <babosz189@gmail.com>
2024-02-11 17:49:52 +0100

    Added SARSA method for TD learning

--- methods/temporal_difference.py
+++ methods/temporal_difference.py
@@ -8,24 +8,90 @@ def one_step_td(mdp: MDP, episodes, step_size: float, policy=None):
     if policy is None:
         policy = 1/len(mdp.env.actions)*np.ones((len(mdp.env.states), len(mdp.env.actions)))
 
+    # Number of steps
+    steps = 1/step_size
+
     # Initialize state-values matrix
     v = np.zeros(len(mdp.env.states), dtype=float)
 
     for _ in range(episodes):
+        step = 0
+
         # Initialize state
         available_states = [state for state in mdp.env.states if
                             state not in mdp.env.terminal_states + mdp.env.obstacle_states]
 
         state = available_states[np.random.choice(len(available_states))]
-        while state not in mdp.env.terminal_states:
-            state_id = mdp.env.states.index(state)
-            action = np.random.choice(mdp.env.actions, p=policy[state_id])
+        while state not in mdp.env.terminal_states and step < steps:
+            s = mdp.env.states.index(state)
+
+            # Choose action given by the policy
+            action = np.random.choice(mdp.env.actions, p=policy[s])
+
+            # Get the next state
             next_state = mdp.get_next_state(state, action)
-            next_state_id = mdp.env.states.index(next_state)
+            sp = mdp.env.states.index(next_state)
+
+            # Get the reward
             reward = mdp.rw(state, action, next_state)
-            v[state_id] = v[state_id] + step_size * (reward + mdp.gamma*v[next_state_id] - v[state_id])
+
+            # Compute the state value
+            v[s] = v[s] + step_size * (reward + mdp.gamma*v[sp] - v[s])
+
+            # Assign the next state as current for next iteration
             state = next_state
 
+            # Episode step increment
+            step += 1
+
     return v
+
+
+def sarsa(mdp: MDP, episodes, step_size: float, epsilon: float):
+    # Number of steps
+    steps = 1/step_size
+
+    # Initialize action-values matrix
+    q = 1/len(mdp.env.actions)*np.ones((len(mdp.env.states), len(mdp.env.actions)))
+
+    for _ in range(episodes):
+        step = 0
+
+        # Initialize state
+        available_states = [state for state in mdp.env.states if
+                            state not in mdp.env.terminal_states + mdp.env.obstacle_states]
+
+        take_action = lambda s: mdp.env.actions[np.argmax(q[s])] if np.random.random() >= epsilon else np.random.choice(mdp.env.actions)
+
+        state = available_states[np.random.choice(len(available_states))]
+        while state not in mdp.env.terminal_states and step < steps:
+            s = mdp.env.states.index(state)
+
+            # Choose action greedily derived from the q values
+            action = take_action(s)
+            a = mdp.env.actions.index(action)
+
+            # Get the next state
+            next_state = mdp.get_next_state(state, action)
+            sp = mdp.env.states.index(next_state)
+
+            # Get the reward
+            reward = mdp.rw(state, action, next_state)
+
+            # Get the next action
+            next_action = take_action(sp)
+            ap = mdp.env.actions.index(next_action)
+
+            # Compute the state-action value
+            q[s][a] = q[s][a] + step_size * (reward + mdp.gamma*q[sp][ap] - q[s][a])
+
+            # Assign the next state as current for next iteration
+            state = next_state
+
+            # Episode step increment
+            step += 1
+
+    return q
+
 
 
--- test/test_temporal_difference.py
+++ test/test_temporal_difference.py
@@ -4,12 +4,31 @@ import numpy as np
 
 import methods.temporal_difference as td
 from envs.grid import Grid
+from envs.grid_a import GridA
 from mdp.markov_decision_process import MDP
 
 
 class TestDP(unittest.TestCase):
 
     def setUp(self):
+        # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+        # # # # # # # # # # # # # # # # # # # # # # # #   G R I D   A   # # # # # # # # # # # # # # # # # # # # # # # #
+        # Set the environment
+        self.grid_a = GridA(5, 5)
+
+        # Define the reward function
+        def gridworld_reward(state: tuple, action, next_state):
+            if next_state == state and (state != (0, 1) and state != (0, 3)):
+                return -1
+            elif state == (0, 1):
+                return 10
+            elif state == (0, 3):
+                return 5
+            else:
+                return 0
+
+        # Instantiate MDP for the environment
+        self.mdp_grid_a = MDP(self.grid_a, gridworld_reward, 0.9)
         # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
         # # # # # # # # # # # # # # # # # # # # # # #   G R I D   4 x 4   # # # # # # # # # # # # # # # # # # # # # # #
         # Set the environment
@@ -23,6 +42,11 @@ class TestDP(unittest.TestCase):
         self.mdp_grid = MDP(self.grid, rw, 1)
 
 
-    def test_optimal_state_value(self):
+    def test_one_step_td(self):
         v = td.one_step_td(self.mdp_grid, 1000, 0.2).reshape(self.grid.grid.shape)
         print(v)
+
+
+    def test_sarsa(self):
+        q = td.sarsa(self.mdp_grid_a, 10000, 0.01, 0.1)
+        print(q)
